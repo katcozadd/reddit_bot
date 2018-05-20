0af78a938a74ae7945143e4ae8221d546e4727c6
katcozadd <krosecozadd@gmail.com>
2018-05-20 16:53:11 -0600

    authentication

--- reddit.py
+++ reddit.py
@@ -2,3 +2,14 @@ import praw #importing the reddit API
 import os #importing miscellaneous operating systems
 import time #importing time access and conversions
 import re #importing regular expressions operator
+
+
+class RedditBot():
+
+
+	def authenticate():
+    """Authenticating User"""
+    print('Authenticating...\n')
+    reddit = praw.Reddit('MomBot', user_agent = 'web:MomBot:v0.1 (by /u/kittycozmo)')
+    print('Authenticated as {}\n'.format(reddit.user.me()))
+    return reddit
