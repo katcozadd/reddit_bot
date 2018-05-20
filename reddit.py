35b6588801fa759e318c23474085575ecd48ffc4
katcozadd <krosecozadd@gmail.com>
2018-05-20 16:45:24 -0600

    setting up environmental variables and base to start working

---
+++ reddit.py
@@ -0,0 +1,4 @@
+import praw #importing the reddit API
+import os #importing miscellaneous operating systems
+import time #importing time access and conversions
+import re #importing regular expressions operator
