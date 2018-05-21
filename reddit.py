3dabee3cf8cad469f0ea98346036087f2cabdf65
katcozadd <krosecozadd@gmail.com>
2018-05-20 20:42:20 -0600

    buggy

--- reddit.py
+++ reddit.py
@@ -6,26 +6,63 @@ import re #importing regular expressions operator
 
 SUBREDDITS = ['test']
 
+PATH = '/Users/katcozadd/Desktop/wdi-work/homework/reddit_bot/comments.txt'
+
 class RedditBot():
 
 	def __init__(self, subreddits, path):
-	"""Initialize the bot."""
-	print("Initializing Bot")
-	self.subreddits = subreddits
-	self.path = path
-	self.main()
+		print("Initializing Bot")
+		self.subreddits = subreddits
+		self.path = path
+		self.main()
 
 
-	def authenticate():
-    """Authenticating User"""
-    print('Authenticating...\n')
-    reddit = praw.Reddit('MomBot', user_agent = 'web:MomBot:v0.1 (by /u/kittycozmo)')
-    print('Authenticated as {}\n'.format(reddit.user.me()))
-    return reddit
+	def authenticate(self):
+	    print('Authenticating...\n')
+	    reddit = praw.Reddit('MomBot', user_agent='web:reddit_bot:v0.1 (by /u/kittycozmo)')
+	    print('Authenticated as {}\n'.format(reddit.user.me()))
+	    return reddit
+
+	def run_mom_bot(reddit, self):
+		print('Getting 20 posts. \n')
+
+		for submission in reddit.subreddit('+'.join(self.subs)).new(limit=20):
+
+			for key, value in self.characters.items():
+
+				match_title = re.findall(key, submission.title)
+				if match_title:
+					print(key + ' found in submission ID: ' + submission.id)
+
+					if not os.path.exists(self.path):
+						open(self.path, 'w').close()
+
+					file_obj_r = open(self.path, 'r')
+
+					if submission.id not in file_obj_r.read().splitlines():
+						print('Link is unique. Posting comment.')
+
+						try:
+							comment = (
+								'Hey mom, I appreciate you')
+
+							submission.reply(comment)
+
+						except praw.exceptions.APIException():
+							print('Waiting 10 minutes. \n')
+							time.sleep(600)
+					else:
+						print('Already replied. No reply needed. \n')
+
+					time.sleep(10)
+
+		print('Waiting 10 minutes. \n')
+		time.sleep(600)
 
 
 	def main(self):
-		"""Running the bot"""
 		reddit = self.authenticate()
 		while True:
-			self.run_bot(reddit)
+			self.run_mom_bot(reddit)
+
+mom_bot = RedditBot(SUBREDDITS, PATH)
