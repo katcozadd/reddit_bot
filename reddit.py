6fc95398339078693f21c95bd79083f356668de4
katcozadd <krosecozadd@gmail.com>
2018-05-20 20:04:44 -0600

    initialize the app and am working on other functionality

--- reddit.py
+++ reddit.py
@@ -4,7 +4,16 @@ import time #importing time access and conversions
 import re #importing regular expressions operator
 
 
+SUBREDDITS = ['test']
+
 class RedditBot():
+
+	def __init__(self, subreddits, path):
+	"""Initialize the bot."""
+	print("Initializing Bot")
+	self.subreddits = subreddits
+	self.path = path
+	self.main()
 
 
 	def authenticate():
@@ -13,3 +22,10 @@ class RedditBot():
     reddit = praw.Reddit('MomBot', user_agent = 'web:MomBot:v0.1 (by /u/kittycozmo)')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
+
+
+	def main(self):
+		"""Running the bot"""
+		reddit = self.authenticate()
+		while True:
+			self.run_bot(reddit)
